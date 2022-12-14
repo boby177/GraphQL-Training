5e0171581a0472a73b0f73c5e7af69cb969522d3
boby177 <bomasu37@gmail.com>
2022-12-14 14:08:18 +0700

    added mutations API for create, update, and delete command

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -25,12 +25,31 @@ const typeDefs = gql`
     movie(name: String!): Movie!
   }
 
+  input CreateUserInput {
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality = INDONESIA
+  }
+
+  input UpdateUsernameInput {
+    id: ID!
+    newUsername: String!
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User
+    updateUsername(input: UpdateUsernameInput!): User
+    deleteUser(id: ID!): User
+  }
+
   enum Nationality {
     CANADA
     BRAZIL
     INDIA
     GERMANY
     CHILE
+    INDONESIA
   }
 `;
 
